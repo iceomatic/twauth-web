df7c1b17531570dec17d6fbe4abfcc3abff3fc2b
Iceomatic <89707822+iceomatic@users.noreply.github.com>
2023-01-08 23:49:54 +0100

    init

--- twauth-web.py
+++ twauth-web.py
@@ -39,7 +39,7 @@ def hello():
 def start():
     # note that the external callback URL must be added to the whitelist on
     # the developer.twitter.com portal, inside the app settings
-    app_callback_url = "https://twitter-auth.herokuapp.com/callback"
+    app_callback_url = url_for('callback', _external=True)
 
     # Generate the OAuth request tokens, then display them
     consumer = oauth.Consumer(
